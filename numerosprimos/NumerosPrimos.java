514a7014718dd64cc8892a83359e15805003c732
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2019-11-29 14:08:06 +0100

    Numeros cambio variables

--- numerosprimos/NumerosPrimos.java
+++ numerosprimos/NumerosPrimos.java
@@ -14,55 +14,55 @@ public class NumerosPrimos {
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-    public static boolean p = false;
+    public static boolean comprobar = false;
 
     public static void main(String arg[]) {
-        int nD = 0;
+        int numeroDigitos = 0;
         int nd = 0;
-        nD = Integer.parseInt(arg[0]);
-        if (nD <= 0) {
+        numeroDigitos = Integer.parseInt(arg[0]);
+        if (numeroDigitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
-            int div = i;
-            int c = 0;
+            int divisor = i;
+            int contador = 0;
 
-            while (div != 0) {
-                div = div / 10;
-                c++;
+            while (divisor != 0) {
+                divisor = divisor / 10;
+                contador++;
             }
-            nd = c;
+            nd = contador;
 
-            if (nd == nD) {
+            if (nd == numeroDigitos) {
                 if (i < 4) {
-                    p = true;
+                    comprobar = true;
                 } else {
                     if (i % 2 == 0) {
-                        p = false;
+                        comprobar = false;
                     } else {
-                        int c1 = 0;
+                        int contador1 = 0;
                         int i1 = 1;
-                        int l = (i - 1) / 2;
-                        if (l % 2 == 0) {
-                            l--;
+                        int acumulacion = (i - 1) / 2;
+                        if (acumulacion % 2 == 0) {
+                            acumulacion--;
                         }
-                        while (i1 <= l) {
+                        while (i1 <= acumulacion) {
                             if (i % i1 == 0) {
-                                c1++;
+                                contador1++;
                             }
                             i1 += 2;
-                            if (c1 == 2) {
-                                i1 = l + 1;
+                            if (contador1 == 2) {
+                                i1 = acumulacion + 1;
                             }
                         }
 
-                        if (c1 == 1) {
-                            p = true;
+                        if (contador1 == 1) {
+                            comprobar = true;
                         }
                     }
                 }
 
-                if (p == true) {
+                if (comprobar == true) {
                     System.out.println(i);
                 }
             }
